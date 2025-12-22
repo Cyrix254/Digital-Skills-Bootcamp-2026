b9380a2a437651173fbcc76ca342d896444b3cf5
Cyrix 254 <edebereagan@gmail.com>
2025-12-22 03:07:27 +0300

    removed scroll arrow

--- digital-skills-bootcamp/src/App.jsx
+++ digital-skills-bootcamp/src/App.jsx
@@ -125,17 +125,7 @@ const App = () => {
           <span className="text-yellow-400 font-bold text-2xl tracking-widest uppercase animate-pulse">
             Scroll to Explore
           </span>
-          <svg 
-            className="w-12 h-12 text-white animate-bounce" 
-            fill="none" 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            strokeWidth="2" 
-            viewBox="0 0 24 24" 
-            stroke="currentColor"
-          >
-            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-          </svg>
+          
         </div>
       </section>
 
